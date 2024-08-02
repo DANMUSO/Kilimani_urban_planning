6fda9602869c5ab023e78801e712c366261e3248
Daniel_Kimani_Muthoni-at-819587518695 <dandevopsgratitudeindia.com>
2024-08-02 06:59:07 +0300

    Kilimani Area

--- kilimani_urban_planning/resources/views/roads.blade.php
+++ kilimani_urban_planning/resources/views/roads.blade.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <!-- Google Maps JavaScript API -->
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDPcXmbNTlOL6p0YwNIpfkZM9xxMn6Vex0"></script>
 
