c91dd03b12d64de308a6c99464d280a51d745815
Daniel_Kimani_Muthoni-at-819587518695 <dandevopsgratitudeindia.com>
2024-08-02 06:58:12 +0300

    Kilimani Area

--- kilimani_urban_planning/resources/views/roads.blade.php
+++ kilimani_urban_planning/resources/views/roads.blade.php
@@ -3,9 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Local Physical Development Plans in Kilimani, Nairobi</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Urban Planning Insights</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Google Maps JavaScript API -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDPcXmbNTlOL6p0YwNIpfkZM9xxMn6Vex0"></script>
+
     <style>
+        #map {
+            height: 500px; /* Set a specific height for the map */
+            width: 100%; /* Set width to 100% */
+        }
+        html, body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
         .card-img-top {
             height: 200px;
             object-fit: cover;
@@ -39,27 +54,15 @@
     </style>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light">
-    <a class="navbar-brand" href="#">
+<div class="header">
         <img src="{{asset('images/logo.png')}}" alt="Logo">
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="{{url('/')}}">Roads</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{url('security')}}">Security</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{url('upcomingprojects')}}">Participate</a>
-            </li>
-        </ul>
+        <h3>Lenana Team</h3>
+        <div class="menu">
+        <a href="{{url('/')}}">Roads</a>
+            <a href="{{url('security')}}">Security</a>
+            <a href="{{url('upcomingprojects')}}">Participate</a>
+        </div>
     </div>
-</nav>
 <div id="map"></div>
 
 <!-- Button trigger modal -->
